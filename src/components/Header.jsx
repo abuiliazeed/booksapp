4d6e28f755d080a4297651be6f2fb922e357b58c
abuiliazeed <abuiliazeed@gmail.com>
2022-06-19 13:45:16 +0200

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div>
+        <div>BooksApp</div>
+        <div>
+          <button>Add a Book</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
