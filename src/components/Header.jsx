e96ecfe42fb7cf0a38db993c62b587fcf2265942
abuiliazeed <abuiliazeed@gmail.com>
2022-06-19 13:56:29 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { FaBook } from "react-icons/fa";
 
 function Header() {
   return (
     <>
-      <div>
-        <div>BooksApp</div>
+      <div className="flex flex-row items-center justify-between p-4 bg-indigo-500">
+        <div className="flex flex-row items-center">
+          <FaBook className="text-white" size={32} />
+          <div className="text-4xl font-bold text-white">BooksApp</div>
+        </div>
+
         <div>
-          <button>Add a Book</button>
+          <button className="bg-white hover:text-lg text-indigo-500 font-bold py-2 px-4 rounded">
+            Add a Book
+          </button>
         </div>
       </div>
     </>
