13aca18ef9a510b3e82ac3564d82d79e1dd21538
abuiliazeed <abuiliazeed@gmail.com>
2022-06-19 14:01:10 +0200

    update

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function BookShelf(props) {
   return (
-    <div>{props.title}</div>
-  )
+    <>
+      <div className="p-4">
+        <div className="text-2xl font-bold text-indigo-600 p-2">{props.title}</div>
+        <div className="border-b-4 border-indigo-300"></div>
+      </div>
+    </>
+  );
 }
 
-export default BookShelf
+export default BookShelf;
