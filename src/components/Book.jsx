d2bc84d148531b1def1cc4702a933bd7134027fb
abuiliazeed <abuiliazeed@gmail.com>
2022-06-20 09:21:38 +0200

    update

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,8 +5,8 @@ function Book(props) {
   return (
     <>
       
-        <div class=" rounded-lg shadow-lg max-w-xs flex flex-col items-center mx-2">
-          <img class="object-scale-down w-80 h-96 " src={props.image} alt="Sunset in the mountains" />
+        <div class=" rounded-lg shadow-lg max-w-xs flex flex-col items-center mx-2 hover:scale-105">
+          <img class="object-cover w-80 h-96 rounded-t-lg" src={props.image} alt="Sunset in the mountains" />
           <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">{props.title}</div>
             <p class="text-gray-700 text-base">{props.authors}</p>
