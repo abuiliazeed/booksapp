f6b80ad7d3262a2f83e517ff238fb246ba1e011a
abuiliazeed <abuiliazeed@gmail.com>
2022-06-20 15:55:22 +0200

    update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,15 @@ import { get, getAll, update, search } from "../BooksAPI.js";
 
 function Search() {
   const [searchTerm, setSearchTerm] = React.useState("");
+  const data = async (searchTerm) => {
+    try {
+      const result = await search(searchTerm);
+      console.log(result);
+    }catch (e) {
+      console.log(e);
+    }
+    
+  }
 
   return (
     <>
@@ -13,7 +22,7 @@ function Search() {
         <button><BiArrowBack  className="p-2 mr-2 text-gray-300 bg-gray-100 h-20 w-20" /></button>
         <input onChange={e => {setSearchTerm(e.target.value)}} placeholder="Search by book title or author" className="w-full h-20 text-gray-500 text-2xl px-2 mx-4 focus:outline-none "></input>
 
-
+        {console.log(data(searchTerm))}
 
      
     
