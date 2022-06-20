206b899a72fd2fec3c691318c571834c21e479b1
abuiliazeed <abuiliazeed@gmail.com>
2022-06-20 09:42:24 +0200

    update

--- src/components/ShelfChanger.jsx
+++ src/components/ShelfChanger.jsx
@@ -6,8 +6,8 @@ function ShelfChanger() {
       <div className="flex justify-center pb-4">
         <div >
           <select
-            className="form-select text-base font-normal text-gray-700 bg-white border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
-            <option selected >Move to</option>
+            className="p-1 form-select text-base font-normal text-gray-700 bg-white border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+            <option selected disabled >Move to</option>
             <option value="1">Currently Reading</option>
             <option value="2">Want to Read</option>
             <option value="3">Read</option>
